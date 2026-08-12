358271fc358ce8dd74a9e6ac624d458fbfb80f5d
Guilherme Macedo <guilhermeap.macedo20@gmail.com>
2026-08-12 16:37:09 -0300

    fix: adjust auth test

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,9 +8,20 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            loginUser: jest.fn(),
+            forgotPassword: jest.fn(),
+            resetPassword: jest.fn(),
+            me: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    controller = module.get(AuthController);
   });
 
   it('should be defined', () => {
